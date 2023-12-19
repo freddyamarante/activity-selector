73593a05bbfcf4a2f24ebac9e1e1296b13e485d4
Freddy Amarante <freddyamarante14@gmail.com>
2023-12-19 15:04:34 -0400

    add submit activity functionality

--- src/Activities/Activities.tsx
+++ src/Activities/Activities.tsx
@@ -1,35 +1,35 @@
 import ActivitiesList from './ActivitiesList'
 import ActivitiesForm from './ActivitiesForm'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 import { ActivityType } from '../types/ActivityType'
 
 export default function Activities() {
-  const [activities, setActivities] = useState<ActivityType[]>([])
+  const [activities, setActivities] = useState<ActivityType[]>([
+    {
+      title: 'Among Us',
+      description: 'Amogus',
+    },
+    {
+      title: 'The Finals',
+      description: 'Full equipos de 3',
+    },
+    {
+      title: 'Omegle',
+      description: 'Ometv broski',
+    },
+  ])
 
-  useEffect(() => {
-    setActivities([
-      {
-        title: 'Among Us',
-        description: 'Amogus',
-      },
-      {
-        title: 'The Finals',
-        description: 'Full equipos de 3',
-      },
-      {
-        title: 'Omegle',
-        description: 'Ometv broski',
-      },
-    ])
-  }, [])
+  const addActivity = (newActivity: ActivityType) => {
+    setActivities((current) => [...current, newActivity])
+  }
 
   return (
-    <div className="flex flex-row space-x-4 px-4">
+    <div className="flex flex-row max-w-3xl mx-auto px-12 justify-between">
       <ActivitiesList activities={activities} />
 
-      <ActivitiesForm />
+      <ActivitiesForm submitButton={addActivity} />
     </div>
   )
 }
--- src/Activities/ActivitiesForm.tsx
+++ src/Activities/ActivitiesForm.tsx
@@ -1,14 +1,44 @@
-export default function ActivitiesForm() {
+import { ActivityType } from '../types/ActivityType'
+
+import { ChangeEvent, FormEvent, useState } from 'react'
+
+type ActivitiesFormProps = {
+  submitButton: (newActivity: ActivityType) => void
+}
+
+export default function ActivitiesForm({ submitButton }: ActivitiesFormProps) {
+  const [formData, setFormData] = useState<ActivityType>({
+    title: '',
+    description: '',
+  })
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }))
+  }
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    submitButton(formData)
+    setFormData({ title: '', description: '' })
+  }
+
   return (
-    <form className="flex flex-col space-y-6">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col flex-grow-0 space-y-6"
+    >
       <div className="flex flex-col">
         <label className="font-semibold text-xl mb-1" htmlFor="title">
           Title
         </label>
         <input
+          id="title"
           name="title"
           type="text"
           placeholder="Title"
+          value={formData.title}
+          onChange={handleChange}
           className="bg-slate-600 border-2 border-slate-950 pl-2 py-1 rounded-lg"
         />
       </div>
@@ -18,14 +48,17 @@ export default function ActivitiesForm() {
           Description
         </label>
         <input
+          id="description"
           name="description"
           type="text"
           placeholder="Description"
+          value={formData.description}
+          onChange={handleChange}
           className="bg-slate-600 border-2 border-slate-950 pl-2 py-1 rounded-lg"
         />
       </div>
 
-      <button className="bg-slate-950 p-2 rounded-lg hover:" type="button">
+      <button type="submit" className="bg-slate-950 p-2 rounded-lg hover:">
         Add Activity
       </button>
     </form>
